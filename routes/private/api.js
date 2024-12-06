6d57d83f5cd14480eeb94e3bb6281a620b582407
MohamedHussienX <mohamed.ma912722@gmail.com>
2024-12-06 23:07:44 +0200

    Equipment View Functions

--- routes/private/api.js
+++ routes/private/api.js
@@ -4,7 +4,27 @@ const { v4 } = require('uuid');
 const { getUser } = require('../../utils/session');
 
 function handlePrivateBackendApi(app) {
+  //M3ana
+  app.post("/api/v1/equipment/new", async (req, res) => {
+    const u= await getUser(req)
+    if(u.role!='admin')
+    {
+     return res.status(400).send("NOT AUTHORIZED");
+    }
+    try{
+      console.log("req",req.body);
+      const {equipmentName, equipmentImgPath, rating, modelNumber,purchaseDate,quantity,status,location} = req.body 
+      const result = await db.raw(
+        `insert into "project"."equipments"(equipmentname, equipmentimgpath, rating, modelnumber,purchasedate,quantity,status,location)
+          values('${equipmentName}','${equipmentImgPath}','${rating}','${modelNumber}','${purchaseDate}','${quantity}','${status}','${location}');`);
+      return res.status(200).send('new equipment has successfully added')
+    }catch(err){
+      console.log("eror message", err.message);
+      return res.status(400).send("failed to add new equipment")
+    }
   
+  });
+  //M3ana
   app.get('/api/v1/users/view' , async function(req , res) {
     const u= await getUser(req)
     if(u.role!='admin')
@@ -13,6 +33,35 @@ function handlePrivateBackendApi(app) {
     }
     try{
       const result = await db.raw(`select * from project.users order by userid`);
+      //console.log(`result here`,result.rows);
+      return res.status(200).send(result.rows);
+    }catch(err){
+      console.log("error message",err.message);
+      return res.status(400).send(err.message);
+    }
+  });   
+  //M3ana
+  app.get('/api/v1/equipment/view' , async function(req , res) {
+    try{
+      const result = await db.raw(`
+            SELECT 
+                e.equipmentname, 
+                e.equipmentimgpath, 
+                e.rating, 
+                e.modelnumber, 
+                e.purchasedate, 
+                e.quantity, 
+                e.status, 
+                e.location, 
+                c.categoryname, 
+                s.suppliername 
+            FROM 
+                "project"."equipments" e
+            JOIN 
+                "project"."categories" c ON e.categoryid = c.categoryid
+            JOIN 
+                "project"."suppliers" s ON e.supplierid = s.supplierid
+        `);
       //console.log(`result here`,result.rows);
       return res.status(200).send(result.rows);
     }catch(err){
@@ -45,11 +94,15 @@ function handlePrivateBackendApi(app) {
     }
   })
   
-  
-  app.delete('/employee/:id', async (req, res)=> {
-    
+  //M3ana
+  app.delete('/api/v1/users/:id', async (req, res)=> {
+    const u= await getUser(req)
+    if(u.role!='admin')
+    {
+     return res.status(400).send("NOT AUTHORIZED");
+    }
     try {
-      const query = `delete from "backendTutorial"."Employee" where id=${req.params.id}`;
+      const query = `delete from "project"."users" where userid=${req.params.id}`;
       const result = await db.raw(query);
       return res.status(200).send("deleted succesfully");
     } catch (err) {
@@ -78,18 +131,21 @@ function handlePrivateBackendApi(app) {
         return res.status(400).send(err.message);
     }
   });   
-  
-  app.put('/employee/:id', async (req, res)=> {
-    
+  //M3ana
+  app.put('/api/v1/users/:id', async (req, res)=> {
+    const u= await getUser(req)
+    if(u.role!='admin')
+    {
+     return res.status(400).send("NOT AUTHORIZED");
+    }
     try {
       //console.log(req.body);
-      const {country , birthdate  , salary } = req.body;
-      console.log(req.body,salary);
-      const query = `update "backendTutorial"."Employee"
-                         set country = '${country}',
-                         salary = '${salary}',
-                         birthdate = '${birthdate}'
-                         where id = ${req.params.id}`
+      const {username , role } = req.body;
+      console.log(req.body);
+      const query = `update "project"."users"
+                         set username = '${username}',
+                         role = '${role}'
+                         where userid = ${req.params.id}`
       const result = await db.raw(query);
       return res.status(200).send("updated succesfully");
     } catch (err) {
